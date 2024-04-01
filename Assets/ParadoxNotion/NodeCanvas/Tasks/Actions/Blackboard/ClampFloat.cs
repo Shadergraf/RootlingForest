b446202068761a77bc20a630b70b4a97e9bc06f5
Christopher Bernal <thevastbernie@gmail.com>
2024-04-01 14:18:08 +0200

    Added NodeCanvas

---
+++ Assets/ParadoxNotion/NodeCanvas/Tasks/Actions/Blackboard/ClampFloat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NodeCanvas.Framework;
+using ParadoxNotion;
+using ParadoxNotion.Design;
+
+
+namespace NodeCanvas.Tasks.Actions
+{
+
+    [Category("✫ Blackboard")]
+    [Description("Clamp a blackboard float variable")]
+    public class ClampFloat : ActionTask
+    {
+
+        [BlackboardOnly]
+        public BBParameter<float> value;
+        public BBParameter<float> clampValueA;
+        public BBParameter<float> clampValueB;
+
+        protected override string info {
+            get { return string.Format("Clamp {0} between {1} and {2}", value, Math.Min(clampValueA.value, clampValueB.value), Math.Max(clampValueA.value, clampValueB.value)); }
+        }
+
+        protected override void OnExecute()
+        {
+            float min = Math.Min(clampValueA.value, clampValueB.value);
+            float max = Math.Max(clampValueA.value, clampValueB.value);
+            value.value = Math.Max(Math.Min(value.value, max), min);
+            EndAction(true);
+        }
+    }
+}
---
+++ Assets/ParadoxNotion/NodeCanvas/Tasks/Actions/Blackboard/ClampInt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NodeCanvas.Framework;
+using ParadoxNotion;
+using ParadoxNotion.Design;
+
+
+namespace NodeCanvas.Tasks.Actions
+{
+
+    [Category("✫ Blackboard")]
+    [Description("Clamp a blackboard int variable")]
+    public class ClampInt : ActionTask
+    {
+
+        [BlackboardOnly]
+        public BBParameter<int> value;
+        public BBParameter<int> clampValueA;
+        public BBParameter<int> clampValueB;
+
+        protected override string info {
+            get { return string.Format("Clamp {0} between {1} and {2}", value, Math.Min(clampValueA.value, clampValueB.value), Math.Max(clampValueA.value, clampValueB.value)); }
+        }
+
+        protected override void OnExecute()
+        {
+            int min = Math.Min(clampValueA.value, clampValueB.value);
+            int max = Math.Max(clampValueA.value, clampValueB.value);
+            value.value = Math.Max(Math.Min(value.value, max), min);
+            EndAction(true);
+        }
+    }
+}
